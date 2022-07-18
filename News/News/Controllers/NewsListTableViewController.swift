bd371d07909cac5fb3c5d65afc1f8534975d9c8d
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-07-18 20:40:03 -0300

    Populando TableView

--- News/News/Controllers/NewsListTableViewController.swift
+++ News/News/Controllers/NewsListTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class NewsListTableViewController: UITableViewController {
     
+    private var articleListVM: ArticleListViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
@@ -21,8 +23,36 @@ class NewsListTableViewController: UITableViewController {
         
         let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=dd228e381c8f4a54b39a14e061e93802")!
         
-        Webservice().getArticles(url: url) { _ in
+        Webservice().getArticles(url: url) { articles in
             
+            if let articles = articles {
+                self.articleListVM = ArticleListViewModel(articles: articles)
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
         }
     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.articleListVM == nil ? 0 : self.articleListVM.numberOfSections
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.articleListVM.numberOfRowsSection(section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else {
+            fatalError("ArticleTableViewCell not found")
+        }
+        
+        let articleVM = self.articleListVM.articleAtIndex(indexPath.row)
+        
+        cell.titleLabel.text = articleVM.title
+        cell.descriptionLabel.text = articleVM.description
+        
+        return cell
+    }
+    
 }
